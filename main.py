c2ceced8ce25e705a8b898992b0ac0f4ad8ff109
Mytrika <105524362+Mytrika-Larito@users.noreply.github.com>
2022-05-13 23:20:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("Welcome to my first game in Python!!!")
+name = input("What is your name? ")
+age = input("What is your age? ")
+print("Hello",name,
+      "Welcome to my game!",
+      "Your are", age, "years old")
