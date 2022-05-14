88a69488d5a302167ea4e409023e130cbb2db35e
Mytrika <105524362+Mytrika-Larito@users.noreply.github.com>
2022-05-14 22:29:48 +0000

    Added age system

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 print ("Welcome to my first game in Python!!!")
 name = input("What is your name? ")
-age = input("What is your age? ")
+age = int(input ("What is your age? "))
 print("Hello",name,
       "Welcome to my game!",
       "Your are", age, "years old")
 
+
+if age >= 12:
+  print("You are old enough")
+
+  want_to_play = input("Do you want to play?(yes or no) ")
+  if want_to_play == "yes":
+    print("Lets get started!")
+
+  else: 
+    print ("Goodbye!")
+    
+elif age <= 0 >= 0:
+  print("Your not alive")
+else:
+  print ("You are not old enough")
