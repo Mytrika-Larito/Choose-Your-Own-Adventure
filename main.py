484b13b996421921aca2be036550942c6f7509e7
Mytrika <105524362+Mytrika-Larito@users.noreply.github.com>
2022-05-15 03:13:02 +0000

    Better comments and more stuff

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ print("Hello",name,
       "Welcome to my game!",
       "Your are", age, "years old")
 
-'''Health'''
+#Health
 health = 10
 
-'''Age System'''
+#Shield
+sh = 0
+
+#Age System
 if age >= 12:
   print("You are old enough")
 
@@ -19,42 +22,74 @@ if age >= 12:
 
     print("You are starting with", health, "health")
 
-    '''Story'''
+    #Story
     print ("You meet yourself in front of a mossy rock with the path you been following for hours diverges. The right path leads to a forest while the left leads to some hilly area, but its to far to see anything else.")
-    l_o_r = input("Do you wanna go left or right (left/right)? ")
+    l_o_r = input("Do you wanna go left or right (left/right)? ").lower()
 
 
     
-    '''Left path'''
+    #Left path
     if l_o_r == "left":
       print ("You followed the path left for 10 minutes. Until you hear a strange noise")
       ll = input("Do you go back and head right?(yes/no) ").lower()
-
+      
       if ll == "yes":
-        l_o_r = "no"
         print ("You decided it was to dangerous. So, you went the other way.")
+        l_o_r = "right"
+        
+        
       elif ll == "no":
-          print("You head towards the noise. Those strange noises were coming from a lively festival in a small village.") 
+          print("You head towards the noise. Those strange noises was coming from a lively festival in a small village.") 
+          print("As you came near the village. You can see the colourful lights and the bright clothing. Filled with many colours and patterns. From men to women wearing long drapes of cloth. Some had colourful earings.")
+          print("You went into the village wandering the place. Until you found yourself with more options.")
+      vil = input("Where would you like to go?(tavern/outside/chief)" ).lower()
+
+elif vil == "tavern":
+        print ("You decided to go into the tavern. You can smell the booze in the air. You can even hear a drunk guy in the corner. Another person is playing a instrument in a shape of a uke. While there is someone who stands out at the bar.")
+tav = input("Who would you like to talk to?(drunkard/uke/bar) ").lower()
+      
+if tav == "drunkard":
+  print ("You went to the drunk guy in the corner. You ask if he was alright. He babbled at you. Saying nothing you can understand. It was pure gibberish in your ears. He could not make a complete sentence.")
+vil == "tavern"
+
+if tav == "uke":
+  print("N/A")
+
+
+if tav == "bar":
+  print("N/A")
+
       
 
-    '''Right path'''
-    if l_o_r == "right":
-      print("You walk into the forest. It suddenly got dark.What do you do. You can barely see whats ahead of you.")
-      rr = input("Do you go back?(yes/no) ").lower()
+      
+if vil == "outside":
+  print("N/A")
 
-    if rr == "yes":
-      print("N/A")
 
-    if rr == "no":
-      health -= 7
-      print("As you went forward. You were attack. Your health is know at", health, "health. You kept on going until you were at a clearing. There was no trees and the sun shine right above you. Though know you find yourself in front of two choices. Both path looks dark and there is no way in telling what each path might have at the end.")
-      rr2 = input("Do you head right or left?(right/left) ").lower()
+if vil == "chief":
+  print("N/A")
 
-      if rr2 == "test":
+        
+      
+
+    #Right path
+  if l_o_r == "right":
+        print("You walk into the forest. It suddenly got dark.What do you do. You can barely see whats ahead of you.")
+  rr = input("Do you go back?(yes/no) ").lower()
+
+  if rr == "yes":
+    print("N/A")
+
+  if rr == "no":
+    health -= 7
+    print("As you went forward. You were attack. Your health is know at", health, "health. You kept on going until you were at a clearing. There was no trees and the sun shine right above you. Though know you find yourself in front of two choices. Both path looks dark and there is no way in telling what each path might have at the end.")
+    rr2 = input("Do you head right or left?(right/left) ").lower()
+
+    if rr2 == "test":
         health -= 3
         print(health,"health")
 
-      if health <= 0:
+    if health <= 0:
         print("You died! ;-;")
 
       
@@ -62,7 +97,7 @@ if age >= 12:
   else: 
     print ("Goodbye!")
 
-    '''Age System'''
+    #Age system
 elif age <= 0 >= 0:
   print("Your not alive")
 else:
